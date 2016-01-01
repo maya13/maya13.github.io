96baa91cf9cc30a3f3bb980c464491f87a320abd
maya13 <maya_imd@hotmail.com>
2016-01-01 00:43:52 -0500

    add js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var main = function (){
+
+	  $('.dropdown-toggie').click(function(){
+	  	$ ('.dropdown-menu').toggle();
+
+});
+
+}
+$ (document).ready(main);
+
+
+
+var main = function(){
+	$('.dropdown-toggle').click(function(){
+	 $('.dropdown-menu').toggle();
+	 
+	}); 	
+}
